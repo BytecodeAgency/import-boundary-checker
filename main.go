611d7261182dc4374cdc4add9023f4d5b9e1ddbb
Luciano Nooijen <luciano@bytecode.nl>
2020-07-02 16:54:12 +0200

    Added config loading
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 
 	"git.bytecode.nl/foss/import-boundry-checker/lexer"
 	"git.bytecode.nl/foss/import-boundry-checker/parser"
@@ -16,7 +18,7 @@ var verbose = false
 func main() {
 	// CLI flags
 	flag.BoolVar(&verbose, "verbose", false, "Enable verbose output of the main function (does not enable debugging for sub packages)")
-	configPath := flag.String("config", "TODO", "Configuration path to be used when building import rule set")
+	configPath := flag.String("config", ".importrules", "Configuration path to be used when building import rule set")
 	printHelp := flag.Bool("help", false, "Print CLI usage information")
 	flag.Parse()
 
@@ -27,18 +29,8 @@ func main() {
 
 	logIfVerbose(fmt.Sprintf("verbose mode set to %b and using configPath %s", verbose, *configPath))
 
-	tester := `LANG "Go";
-
-IMPORTRULE "git.bytecode.nl/single-projects/youngpwr/platform-backend/typings/entities"
-CANNOTIMPORT "git.bytecode.nl/single-projects/youngpwr/platform-backend";
-
-IMPORTRULE
-  	"git.bytecode.nl/single-projects/youngpwr/platform-backend/domain"
-CANNOTIMPORT
-	"git.bytecode.nl/single-projects/youngpwr/platform-backend/infrastructure"
-    "git.bytecode.nl/single-projects/youngpwr/platform-backend/data";`
-
-	l := doLex(tester)
+	c := getConfigString(*configPath)
+	l := doLex(c)
 	p, lang := doParse(l)
 
 	// DEV
@@ -46,6 +38,22 @@ CANNOTIMPORT
 	for _, pp := range p {
 		log(fmt.Sprintf("  > %+v", pp))
 	}
+}
+
+func getConfigString(path string) string {
+	logIfVerbose("Creating fullPath")
+	abs, err := filepath.Abs(path)
+	if err != nil {
+		fail("Could not create absolute config file path " + err.Error())
+	}
+	fullPath := abs
+	logIfVerbose("fullPath = " + fullPath)
+	config, err := ioutil.ReadFile(fullPath)
+	logIfVerbose("config = " + string(config))
+	if err != nil {
+		fail(fmt.Sprintf("Could not read config file contents (%s), err %s", config, err))
+	}
+	return string(config)
 }
 
 func doLex(input string) []lexer.Result {
