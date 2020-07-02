18e6205d85aebee952b6673a0a5cc1e674455338
Luciano Nooijen <luciano@bytecode.nl>
2020-07-02 17:26:41 +0200

    WIP on import extractor for Go
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

---
+++ langs/golistimports/golistimports.go
@@ -0,0 +1,7 @@
+package golistimports
+
+func ExtractForSourceFile(source string) []string {
+	// TODO: Parse source file, find line beginning with `import (` or `import "`
+	// TODO: Implement
+	return []string{source}
+}
---
+++ langs/golistimports/golistimports_test.go
@@ -0,0 +1,62 @@
+package golistimports_test
+
+import (
+	"testing"
+
+	"git.bytecode.nl/foss/import-boundry-checker/langs/golistimports"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExtractForSourceFile(t *testing.T) {
+	tests := []struct {
+		input           string
+		expectedImports []string
+	}{
+		{`package user
+
+import (
+	"github.com/bytecodeagency/proj/typings/entities"
+	"github.com/bytecodeagency/typings/interactors"
+	"github.com/go-playground/validator/v10"
+)
+
+type DomainInstance interface {
+	// CRUD
+	Importer(tester string) error
+	CreateUser(user entities.NewUser) (jwt string, err error)
+
+	// Authentication
+	AuthenticateUser(email string, password string) (jwt *string, err error)
+	CheckUserJwt(jwt string) (user *entities.User, err error)
+
+	// Password reset
+	StartPasswordReset(email string) error
+	SavePasswordReset(resetToken string, password string) error
+}
+
+type userDomain struct {
+	interactors.DomainInteractor
+}
+
+func NewUserDomainInstance(interactor interactors.DomainInteractor) (DomainInstance, error) {
+	domain := userDomain{
+		interactor,
+	}
+	validate := validator.New()
+	err := validate.Struct(domain)
+	return domain, err
+}`, []string{"github.com/bytecodeagency/typings/entities", "github.com/bytecodeagency/typings/interactors", "github.com/go-playground/validator/v10"}},
+		{`package sometest
+
+import "github.com/bytecodeagency/typings/entities"
+
+func importer () int {
+  return 0
+}
+`, []string{"github.com/bytecodeagency/typings/entities"}},
+	}
+	for _, test := range tests {
+		imports := golistimports.ExtractForSourceFile(test.input)
+		assert.Equal(t, test.expectedImports, imports)
+	}
+}
