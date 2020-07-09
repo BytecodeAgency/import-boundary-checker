63c4d461c99691ec7bff67fe16249017d8478072
Luciano Nooijen <luciano@bytecode.nl>
2020-07-09 14:19:33 +0200

    Added rulechecker
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

---
+++ rulechecker/rulechecker.go
@@ -0,0 +1,63 @@
+package rulechecker
+
+import (
+	"strings"
+
+	"git.bytecode.nl/foss/import-boundry-checker/parser"
+)
+
+type ImportChart map[string][]string
+
+type Violation struct {
+	Filename     string
+	CannotImport string
+	ImportLine   string
+}
+
+type RuleChecker struct {
+	Rules       []parser.Rule
+	ImportChart ImportChart
+	Violations  []Violation
+}
+
+func New(rules []parser.Rule, importChart ImportChart) RuleChecker {
+	return RuleChecker{
+		Rules:       rules,
+		ImportChart: importChart,
+	}
+}
+
+func (rc *RuleChecker) Check() (isValid bool) {
+	for file, imports := range rc.ImportChart {
+		forbiddenImports := rc.findForbiddenImportsForFilename(file)
+		for _, importLine := range imports {
+			for _, forbiddenImport := range forbiddenImports {
+				if strings.HasPrefix(importLine, forbiddenImport) {
+					rc.addViolation(file, importLine, forbiddenImport)
+				}
+			}
+		}
+	}
+	return len(rc.Violations) == 0
+}
+
+func (rc *RuleChecker) findForbiddenImportsForFilename(fileName string) []string {
+	var forbiddenImports []string
+	for _, rule := range rc.Rules {
+		if strings.HasPrefix(fileName, rule.RuleFor) {
+			for _, forbiddenImport := range rule.CannotImport {
+				forbiddenImports = append(forbiddenImports, forbiddenImport)
+			}
+		}
+	}
+	return forbiddenImports
+}
+
+func (rc *RuleChecker) addViolation(filename string, importLine string, cannotImport string) {
+	violation := Violation{
+		Filename:     filename,
+		CannotImport: cannotImport,
+		ImportLine:   importLine,
+	}
+	rc.Violations = append(rc.Violations, violation)
+}
---
+++ rulechecker/rulechecker_test.go
@@ -0,0 +1,38 @@
+package rulechecker_test
+
+import (
+	"testing"
+
+	"git.bytecode.nl/foss/import-boundry-checker/parser"
+	"git.bytecode.nl/foss/import-boundry-checker/rulechecker"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRuleChecker_Check(t *testing.T) {
+	tests := []struct {
+		expectedResult bool
+		rules          []parser.Rule
+		importChart    rulechecker.ImportChart
+	}{
+		{
+			true,
+			[]parser.Rule{{"package/domain", []string{"package/data"}}},
+			map[string][]string{"package/domain/file.go": {"fmt", "package/typings/entities"}},
+		},
+		{
+			false,
+			[]parser.Rule{{"package/domain", []string{"package/data", "ioutil"}}},
+			map[string][]string{"package/domain/file.go": {"fmt", "ioutil"}},
+		},
+		{
+			false,
+			[]parser.Rule{{"package/domain", []string{"package/data"}}},
+			map[string][]string{"package/domain/file.go": {"fmt", "package/data"}},
+		},
+	}
+	for _, test := range tests {
+		rc := rulechecker.New(test.rules, test.importChart)
+		res := rc.Check()
+		assert.Equal(t, res, test.expectedResult)
+	}
+}
